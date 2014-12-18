f0e5e6191aa4cda818fd757a0b90e5a3e6c5f00b
Dhananjoy Biswas <ebappa1971@gmail.com>
2014-12-19 00:16:46 +0600

    modified:   Library/Utility.cpp

--- Library/Utility.cpp
+++ Library/Utility.cpp
@@ -1,20 +1,36 @@
+/*
+    Implementation of binary arithmetic operators from uninary arithmetic operators
+
+    Usage:
+        using namespace arithmetic_ops;
+        struct A {
+            ...
+            A& operator += (const B& rhs) { ... }
+        };
+        ...
+        A lhs;
+        B rhs;
+        A sum = lhs + rhs;
+
+*/
+
 namespace arithmetic_ops {
-    template <class T, class E> T operator + (T lhs, const E& rhs) {
+    template <class L, class R> L operator + (L lhs, const R& rhs) {
         lhs += rhs;
         return lhs;
     }
 
-    template <class T, class E> T operator - (T lhs, const E& rhs) {
+    template <class L, class R> L operator - (L lhs, const R& rhs) {
         lhs += rhs;
         return lhs;
     }
 
-    template <class T, class E> T operator * (T lhs, const E& rhs) {
+    template <class L, class R> L operator * (L lhs, const R& rhs) {
         lhs += rhs;
         return lhs;
     }
 
-    template <class T, class E> T operator / (T lhs, const E& rhs) {
+    template <class L, class R> L operator / (L lhs, const R& rhs) {
         lhs += rhs;
         return lhs;
     }
